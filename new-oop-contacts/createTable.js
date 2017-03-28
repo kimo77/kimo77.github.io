c5d7c4478b04b8b02cecf0674ca4f8947299818a
kimo77 <77kimo@gmail.com>
2017-03-28 10:44:25 +0300

    Added google map

--- new-oop-contacts/createTable.js
+++ new-oop-contacts/createTable.js
@@ -6,8 +6,8 @@ contactApp.tableUpdate = function () {
             '<td>' + contact.firstName + '</td>' +
             '<td>' + contact.lastName + '</td>' +
             '<td>' + contact.phone + '</td>' +
-            '<td>' + contact.streetAddress + '</td>' +
-            '<td>' + contact.city + '</td>' +
+            '<td><a href="http://maps.google.com/?q=' + contact.streetAddress + ' ' + contact.city + '">' + contact.streetAddress + ' ' + contact.city + '</a></td>' +
+            //'<td>' + contact.city + '</td>' +
             '<td><button class="contactEditBtn mdl-button mdl-js-button mdl-button--raised mdl-button--accent" onclick="contactApp.editContact(' + contact.id + ')">edit</button></td>' +
             '<td><button class="contactDelBtn mdl-button mdl-js-button mdl-button--raised mdl-button--accent" onclick="contactApp.deleteContact(' + contact.id + ')">del</button></td>' +
             '</tr>');
